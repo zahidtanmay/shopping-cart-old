47b744804a8daf1a65a8d12b929d394885c7cf00
Jahid <zahidtanmay009@gmail.com>
2017-05-28 21:50:33 +0600

    First Phase

---
+++ resources/views/shop/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.master')
+
+@section('title')
+Laravel Shopping Cart
+@endsection
+@section('content')
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <img src="https://www.familyeducation.com/sites/default/files/styles/slideshow_image_802x451_/public/collection-item/HPgerman3_TV.gif?itok=UB82zmQD" alt="..." class="image-responsive">
+      <div class="caption">
+        <h3>Product Title</h3>
+        <p class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nisi odio, laoreet vitae nisi vitae, pretium cursus lorem. Suspendisse sed eleifend eros. Nullam porta vel dolor fringilla posuere.</p>
+        <div class="pull-left price">$12</div>
+        <div class="clearfix"> <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a></div>
+      </div>
+    </div>
+  </div>
+  
+</div>
+@endsection
